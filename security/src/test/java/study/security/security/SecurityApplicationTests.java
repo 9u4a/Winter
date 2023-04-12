451ca9233893117d3b8163f46781899548602ba8
9u4a <99junsung@gmail.com>
2023-04-12 15:02:46 +0900

    [ Security ] Create project Security

---
+++ security/src/test/java/study/security/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package study.security.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
